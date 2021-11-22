8da10a255230bb3204ec09bd105e9d1a88822343
Carl G <griffiniiicarl@gmail.com>
2021-11-22 15:29:12 -0600

    deploy fix 4

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
 import emailjs from 'emailjs-com';
 
 interface ContactProps {
